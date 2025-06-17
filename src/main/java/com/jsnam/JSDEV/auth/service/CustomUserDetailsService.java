1ffc2ec07015446928243772db35df34a460ee3a
jsnam <skawotkd12@naver.com>
2025-06-17 18:20:58 +0900

    수정

--- src/main/java/com/jsnam/JSDEV/auth/service/CustomUserDetailsService.java
+++ src/main/java/com/jsnam/JSDEV/auth/service/CustomUserDetailsService.java
@@ -15,23 +15,22 @@ import org.springframework.stereotype.Service;
 public class CustomUserDetailsService implements UserDetailsService {
 
     private final MemberRepository memberRepository;
-//    private final PasswordEncoder passwordEncoder;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        Member member = memberRepository.findByEmail(email)
+        return memberRepository.findByEmail(email)
+//                .map(this::createUserDetails)
                 .orElseThrow(() -> new UsernameNotFoundException("해당하는 회원을 찾을 수 없습니다."));
-        return member; // Member가 UserDetails를 implements하므로 가능
     }
 
     // 해당하는 User 의 데이터가 존재한다면 UserDetails 객체로 만들어서 return
-//    private UserDetails createUserDetails(Member member) {
-//        return User.builder()
-//                .username(member.getEmail())
-//                // 패스워드 디코딩 해야함 임시로 인코딩
-//                .password(passwordEncoder.encode(member.getPassword()))
-//                .roles(member.getRole())
-//                .build();
-//    }
+    private UserDetails createUserDetails(Member member) {
+        return User.builder()
+                .username(member.getEmail())
+                .password(passwordEncoder.encode(member.getPassword()))
+                .roles(member.getRole())
+                .build();
+    }
 
 }
